b9c35baded2e6adc05dafa0f6c02e5ad60dc0351
Foelli <159255468+Foelli@users.noreply.github.com>
2025-11-21 10:57:24 +0100

    Initial Commit

---
+++ GiftShift/AVPlayerView.swift
@@ -0,0 +1,20 @@
+//
+//  AVPlayerView.swift
+//  GiftShift
+//
+//  Created by Simon Felhofer on 21.11.25.
+//
+
+import SwiftUI
+
+struct AVPlayerView: UIViewControllerRepresentable {
+    let viewModel: AVPlayerViewModel
+
+    func makeUIViewController(context: Context) -> some UIViewController {
+        return viewModel.makePlayerViewController()
+    }
+
+    func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
+        // Update the AVPlayerViewController as needed
+    }
+}
---
+++ GiftShift/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  GiftShift
+//
+//  Created by Simon Felhofer on 21.11.25.
+//
+
+import SwiftUI
+import RealityKit
+
+struct ContentView: View {
+
+    var body: some View {
+        VStack {
+            ToggleImmersiveSpaceButton()
+        }
+    }
+}
+
+#Preview(windowStyle: .automatic) {
+    ContentView()
+        .environment(AppModel())
+}
